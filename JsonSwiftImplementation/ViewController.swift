7aea14938e225da40ee13d511f72ffdaa9236a28
Your Name <your_email@whatever.com>
2017-12-15 17:15:06 +0530

    Demo with Json swifty

--- JsonSwiftImplementation/ViewController.swift
+++ JsonSwiftImplementation/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +22,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
 
